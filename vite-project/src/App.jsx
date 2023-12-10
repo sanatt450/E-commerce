5668d4ccc002222767d6b638759233cb1fbeaa76
SANATT KUMAR MISHRA <73418059+sanatt450@users.noreply.github.com>
2023-12-10 16:40:33 +0000

    folder update

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom"
 
+import Home from './pages/home/Home'
+import Order from './'
 const App = () => {
   return (
     <div>App</div>
---
+++ vite-project/src/pages/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import layout from '../../components/layout/layout'
+const Home = () => {
+  return (
+    <layout>Home</layout>
+  )
+}
+
+export default Home
